6a6ec0549e172252ba521fd4bcaedfdb977d9780
Aleksandr <aleksandr@kewb.io>
2023-03-17 16:13:32 +0200

    Add calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,55 +1,58 @@
-import Vue from 'vue';
-import Base from './base';
-import axios from 'axios';
-import Routes from './routes';
-import VueRouter from 'vue-router';
-import VueJsonPretty from 'vue-json-pretty';
-import 'vue-json-pretty/lib/styles.css';
-import moment from 'moment-timezone';
+import Vue from 'vue'
+import Base from './base'
+import axios from 'axios'
+import Routes from './routes'
+import VueRouter from 'vue-router'
+import VueJsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css'
+import moment from 'moment-timezone'
+import VCalendar from 'v-calendar'
 
-require('bootstrap');
+require('bootstrap')
 
-let token = document.head.querySelector('meta[name="csrf-token"]');
+let token = document.head.querySelector('meta[name="csrf-token"]')
 
 if (token) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
 }
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-window.Popper = require('popper.js').default;
+window.Popper = require('popper.js').default
 
-moment.tz.setDefault(Telescope.timezone);
+moment.tz.setDefault(Telescope.timezone)
 
-window.Telescope.basePath = '/' + window.Telescope.path;
+window.Telescope.basePath = '/' + window.Telescope.path
 
-let routerBasePath = window.Telescope.basePath + '/';
+let routerBasePath = window.Telescope.basePath + '/'
 
 if (window.Telescope.path === '' || window.Telescope.path === '/') {
-    routerBasePath = '/';
-    window.Telescope.basePath = '';
+    routerBasePath = '/'
+    window.Telescope.basePath = ''
 }
 
 const router = new VueRouter({
     routes: Routes,
     mode: 'history',
     base: routerBasePath,
-});
+})
 
-Vue.component('vue-json-pretty', VueJsonPretty);
-Vue.component('related-entries', require('./components/RelatedEntries.vue').default);
-Vue.component('index-screen', require('./components/IndexScreen.vue').default);
-Vue.component('preview-screen', require('./components/PreviewScreen.vue').default);
-Vue.component('alert', require('./components/Alert.vue').default);
+Vue.component('vue-json-pretty', VueJsonPretty)
+Vue.component('related-entries', require('./components/RelatedEntries.vue').default)
+Vue.component('index-screen', require('./components/IndexScreen.vue').default)
+Vue.component('preview-screen', require('./components/PreviewScreen.vue').default)
+Vue.component('alert', require('./components/Alert.vue').default)
 
-Vue.mixin(Base);
+Vue.use(VCalendar, {})
+
+Vue.mixin(Base)
 
 new Vue({
     el: '#telescope',
 
     router,
 
-    data() {
+    data () {
         return {
             alert: {
                 type: null,
@@ -60,33 +63,43 @@ new Vue({
             },
 
             autoLoadsNewEntries: localStorage.autoLoadsNewEntries === '1',
-
+            filtersEntriesByDate: localStorage.filtersEntriesByDate === '1',
             recording: Telescope.recording,
-        };
+        }
     },
 
     methods: {
-        autoLoadNewEntries() {
+        autoLoadNewEntries () {
             if (!this.autoLoadsNewEntries) {
-                this.autoLoadsNewEntries = true;
-                localStorage.autoLoadsNewEntries = 1;
+                this.autoLoadsNewEntries = true
+                localStorage.autoLoadsNewEntries = 1
             } else {
-                this.autoLoadsNewEntries = false;
-                localStorage.autoLoadsNewEntries = 0;
+                this.autoLoadsNewEntries = false
+                localStorage.autoLoadsNewEntries = 0
             }
         },
 
-        toggleRecording() {
-            axios.post(Telescope.basePath + '/telescope-api/toggle-recording');
-
-            window.Telescope.recording = !Telescope.recording;
-            this.recording = !this.recording;
+        filterEntriesByDate () {
+            if (!this.filtersEntriesByDate) {
+                this.filtersEntriesByDate = true
+                localStorage.filtersEntriesByDate = 1
+            } else {
+                this.filtersEntriesByDate = false
+                localStorage.filtersEntriesByDate = 0
+            }
         },
 
-        clearEntries() {
+        toggleRecording () {
+            axios.post(Telescope.basePath + '/telescope-api/toggle-recording')
+
+            window.Telescope.recording = !Telescope.recording
+            this.recording = !this.recording
+        },
+
+        clearEntries () {
             if (confirm('Are you sure you want to delete all Telescope data?')) {
-                axios.delete(Telescope.basePath + '/telescope-api/entries').then((response) => location.reload());
+                axios.delete(Telescope.basePath + '/telescope-api/entries').then((response) => location.reload())
             }
         },
     },
-});
+})
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -56,6 +56,18 @@
                 </svg>
             </button>
 
+            <v-date-picker
+                mode='range'
+                v-model="range"
+                :popover="{ placement: 'bottom', visibility: 'click' }">
+                <button class="btn btn-outline-primary mr-3" :class="{active: filtersEntriesByDate}" v-on:click.prevent="filterEntriesByDate" title="Filter entries by date range">
+                    <svg class="bi bi-calendar3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+                        <path fill-rule="evenodd" d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                    </svg>
+                </button>
+            </v-date-picker>
+
             <router-link to="/monitored-tags" class="btn btn-muted d-flex align-items-center py-2" title="Monitoring">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon" fill="currentColor">
                     <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
